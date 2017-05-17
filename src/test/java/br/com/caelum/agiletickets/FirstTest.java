c5485e1e523e2de6e819e9ffbe1b930d0ec7b86b
rafaelsclaus <rafaelclaus331@gmail.com>
2017-05-16 21:01:08 -0300

    corrigindo bug de numero de ingressos igual ao numero de vagas

--- src/test/java/br/com/caelum/agiletickets/FirstTest.java
+++ src/test/java/br/com/caelum/agiletickets/FirstTest.java
@@ -4,11 +4,22 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import br.com.caelum.agiletickets.models.Sessao;
+import junit.framework.Assert;
+
 public class FirstTest {
 
 	@Test
 	public void hello() {
 		assertTrue(true);
+	}
+	
+	@Test
+	public void deveVender2IngressosSeHa2Vagas(){
+		Sessao sessao = new Sessao();
+		sessao.setTotalIngressos(2);
+		
+		Assert.assertTrue(sessao.podeReservar(2));
 	}
 	
 }
