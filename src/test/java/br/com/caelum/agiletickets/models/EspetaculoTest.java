10e919f3879fd696267a97f626752d5bb223f73f
rafaelsclaus <rafaelclaus331@gmail.com>
2017-05-18 22:22:18 -0300

    Teste Jenkins

--- src/test/java/br/com/caelum/agiletickets/models/EspetaculoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/EspetaculoTest.java
@@ -126,7 +126,7 @@ public class EspetaculoTest {
 		Espetaculo espetaculo = new Espetaculo();
 
 		LocalDate inicio = new LocalDate();
-		LocalDate fim = new LocalDate().plusWeeks(10);
+		LocalDate fim = new LocalDate().plusWeeks(3);
 
 		List<Sessao> sessoes = espetaculo.criaSessoes(inicio, fim, new LocalTime(), Periodicidade.SEMANAL);
 
